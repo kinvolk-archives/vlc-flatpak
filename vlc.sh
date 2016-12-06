3a8d8a33a02dfa92e374cdb19668d34a36d4ac7d
Michal Rostecki <michal@kinvolk.io>
2016-12-06 16:03:49 +0100

    Use --extra-data for patented codecs
    Fixes #3

--- vlc.sh
+++ vlc.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-exec /app/extra/bin/vlc
+extend_ld_path () {
+	export LD_LIBRARY_PATH="$(find $1 -type d | tr '\n' ':')$LD_LIBRARY_PATH"
+}
+
+extend_ld_path /app/lib
+extend_ld_path /app/extra/lib
+
+exec /app/bin/vlc
