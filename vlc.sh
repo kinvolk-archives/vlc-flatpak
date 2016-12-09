acf2893c4877d9f4c81ffe9898a74e4014414e68
Michal Rostecki <michal@kinvolk.io>
2016-12-09 10:13:07 +0100

    Use debs only for non-free and patented content as extra-data
    Using debian packages for free components wasn't good
    and was only done for a PoC. This change builds all free
    dependencies from upstream tarballs and moves debs for
    non-free components to --extra-data args.

--- vlc.sh
+++ vlc.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-extend_ld_path () {
-	export LD_LIBRARY_PATH="$(find $1 -type d | tr '\n' ':')$LD_LIBRARY_PATH"
-}
-
-extend_ld_path /app/lib
-extend_ld_path /app/extra/lib
-
-exec /app/bin/vlc
+exec /app/extra/bin/vlc
